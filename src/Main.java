2c4fa5ab8228a2a3b12143c470bf95c46d46b38c
Neo <ln24630@gmail.com>
2021-03-08 22:58:22 -0800

    Adding input functions for the main for database connection

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,35 @@
  *
  * @author Chunchunmaru
  */
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
+    static String USER;
+    static String PASS;
+    static String DBNAME;
+    
+    static final String displayFormat="%-5s%-15s%-15s%-15s\n";
+    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
+    static String DB_URL = "jdbc:derby://localhost:1527/";
+    
+    
+    public static void main(String[] args){
+        UserInterface UIobj = new UserInterface();
+
+        //Asking for DB name
+        getDBname();
+    }
+    
+    //fixme this works even for no database name;
+    public static String getDBname(){
+        UserInterface UIobj = new UserInterface();
+        String user_input = "-1";
+        while (user_input == "-1" || user_input.length() < 1){
+            UIobj.printDBprompt();
+            user_input = UIobj.getUserInput();
+        }
+        return user_input;
+    }
     
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,6 +8,34 @@
  *
  * @author Chunchunmaru
  */
+import java.util.Scanner;
 public class UserInterface {
+    
+    public void printNameprompt(){
+        System.out.println("Enter a username: ");
+    }
+    
+    public void printDBprompt(){
+        System.out.println("Enter a database name: ");
+    }
+    
+    public void printPassprompt(){
+        System.out.println("Enter a password: ");
+    }
+        
+    public String getUserInput(){
+        Scanner in = new Scanner(System.in);
+        String user_input = "";
+        try{
+            user_input = in.nextLine();
+            return user_input;
+        }
+        catch (Exception e){
+            System.out.println("Invalid input, please try again.");
+            return "-1";
+        }
+    }
+    
+
     
 }
