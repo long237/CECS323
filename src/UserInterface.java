1f1d8ff6d4370ba0cc3f685e0ea29d6d02918e63
Lunastra <ln24630@gmail.com>
2021-03-16 18:07:42 -0700

    add a new printResult but doens't look good

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -80,10 +80,38 @@ public class UserInterface {
             }
         } catch (SQLException se) {
             // Handle errors for JDBC
-            se.printStackTrace();
+            //se.printStackTrace();
+            System.out.println("Error when printing result set!");
         }
 
     }
+    
+    //Better looking print function
+//    public void printResultSet(ResultSet rs) {
+//        try {
+//            ResultSetMetaData rsmd = rs.getMetaData();
+//            int numCol = rsmd.getColumnCount();
+//            String space = String.format("%-" + 15 + "s", "");
+//            String row_sp = String.format("%-" + 20 + "s", "");
+//            for (int j = 1; j <= numCol; j++){
+//                System.out.print(rsmd.getColumnName(j));
+//                System.out.print(space);
+//            }
+//            System.out.println("");
+//            while (rs.next()) {
+//                for (int i = 1; i <= numCol; i++) {
+//                    String colValue = rs.getString(i);
+//                    System.out.print(colValue);
+//                    System.out.print(row_sp);
+//                }
+//                System.out.println("");
+//            }
+//        } catch (SQLException se) {
+//            // Handle errors for JDBC
+//            //se.printStackTrace();
+//            System.out.println("Error when printing result set!");
+//        }
+//    }
 
     // List all writing groups
     public ResultSet listWritingGroup(Connection conn) {
